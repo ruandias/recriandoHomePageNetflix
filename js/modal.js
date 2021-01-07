1f86bf66f27cd54a62a3021bea195b07acb5f40e
ruandias <ruan_ndias@hotmail.com>
2021-01-07 10:52:08 -0300

    adicionado modal pop up quando clicar no botão 'ASSISTIR AGORA'

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,21 @@
 function iniciaModal(modalId) {
-  const modal = document.getElementById(modalId)
+  let modal = document.getElementById(modalId)
   modal.classList.add('mostrar');
   modal.addEventListener('click', function(evento) {
-    console.log(modalId);
-    console.log(evento.target.id);
-    if(evento.target.id == modalId || evento.target.id == 'fecharModal') {
-      modal.classList.remove('mostrar');
-    }
+    let botaoFechar = evento.target.id;
+    if(evento.target.id == modalId || evento.target.id == botaoFechar) {
+    modal.classList.remove('mostrar');
+  }
   });
 }
 
-const maisInformacoes = document.getElementById('abrirModal');
+const maisInformacoes = document.getElementById('abrirModalInfo');
 maisInformacoes.addEventListener('click', function() {
   iniciaModal('modal-informacoes');
+});
+
+
+const assistir = document.getElementById('abrirModalAssistir');
+assistir.addEventListener('click', function() {
+  iniciaModal('modal-assistir');
 });
