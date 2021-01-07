2ac3fe16ecdbdd4582039d4ed35723c609be373f
ruandias <ruan_ndias@hotmail.com>
2021-01-06 23:33:09 -0300

    adicionado funcionalidade para fechar modal ao clicar no botão 'X'

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,16 @@
 function iniciaModal(modalId) {
   const modal = document.getElementById(modalId)
   modal.classList.add('mostrar');
+  modal.addEventListener('click', function(evento) {
+    console.log(modalId);
+    console.log(evento.target.id);
+    if(evento.target.id == modalId || evento.target.id == 'fecharModal') {
+      modal.classList.remove('mostrar');
+    }
+  });
 }
 
-const maisInformacoes = document.getElementById('abriModal');
+const maisInformacoes = document.getElementById('abrirModal');
 maisInformacoes.addEventListener('click', function() {
   iniciaModal('modal-informacoes');
 });
