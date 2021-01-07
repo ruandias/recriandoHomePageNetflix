5e0156c676f25c1560578dbe8ec6c1b67bf7575b
ruandias <ruan_ndias@hotmail.com>
2021-01-07 12:38:02 -0300

    adicionado funcionalidade para parar o vídeo quando clicar no botão fechar

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,42 @@
 function iniciaModal(modalId) {
-  let modal = document.getElementById(modalId)
-  modal.classList.add('mostrar');
-  modal.addEventListener('click', function(evento) {
-    let botaoFechar = evento.target.id;
-    if(evento.target.id == modalId || evento.target.id == botaoFechar) {
-    modal.classList.remove('mostrar');
+  let botaoFechar = "";
+  if (modalId == "modal-informacoes") {
+    botaoFechar = "fecharModalInfo";
   }
+  if (modalId == "modal-assistir") {
+    botaoFechar = "fecharModalAssistir";
+  }
+  console.log(botaoFechar);
+
+  let modal = document.getElementById(modalId);
+  modal.classList.add('mostrar');
+
+  modal.addEventListener('click', function (evento) {
+    console.log(evento);
+    if (evento.target.id == modalId || evento.target.id == botaoFechar) {
+
+      if (modalId == "modal-assistir") {
+        pararVideo(modal);
+
+      }
+
+      modal.classList.remove('mostrar');
+    }
   });
 }
 
+function pararVideo(modal) {
+  let iframeAtual = modal.querySelector('.modal-conteudo > iframe');
+  iframeAtual.src = iframeAtual.src;
+}
+
 const maisInformacoes = document.getElementById('abrirModalInfo');
-maisInformacoes.addEventListener('click', function() {
+maisInformacoes.addEventListener('click', function () {
   iniciaModal('modal-informacoes');
 });
 
 
 const assistir = document.getElementById('abrirModalAssistir');
-assistir.addEventListener('click', function() {
+assistir.addEventListener('click', function () {
   iniciaModal('modal-assistir');
 });
